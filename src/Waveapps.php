ff2e58e555e3962cfbc746fad72080c89d34aa01
samyak <tuladharsamyak@gmail.com>
2026-08-05 05:56:11 +0545

    guzzle http version upgraded

--- src/Waveapps.php
+++ src/Waveapps.php
@@ -26,7 +26,6 @@ class Waveapps
 
     public function __construct($client = null, $graphqlUrl = null, $token = null, $businessId = null, array $config = [])
     {
-	dd($config);
         $this->config = $config;
 
         $this->token = ($token ? $token : $config['access_token']);
